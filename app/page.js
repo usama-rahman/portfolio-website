03da6e720f650db5d24dbbe41359b0ef93f61a0b
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-07 19:25:59 +0600

    About

--- app/page.js
+++ app/page.js
@@ -1,17 +1,20 @@
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import About from "@/components/about/About";
 import Projects from "@/components/projects/Projects";
 
 export default function Home() {
   return (
     <main
-      style={{ backgroundImage: "url(/Stars.png)" }}
-      className="relative flex flex-col justify-between w-full min-h-screen bg-black "
+      // style={{ backgroundImage: "url(/Stars.png)" }}
+      // className="relative flex flex-col justify-between w-full min-h-screen bg-black "
+      className="bg-gray-300"
     >
       <Navbar />
       <Hero />
       <Projects />
+      <About />
       <Footer />
     </main>
   );
---
+++ components/about/About.jsx
@@ -0,0 +1,22 @@
+"use client";
+// import Paragraph from "@/components/Paragraph";
+import Word from "./Word";
+// import Character from "@/components/Character";
+
+const paragraph =
+  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.";
+
+export default function Home() {
+  const words = paragraph.split(" ");
+  return (
+    <main>
+      {/* <div style={{ height: "100vh" }}></div> */}
+      {/* <Paragraph paragraph={paragraph} /> */}
+      {/* <div style={{ height: "100vh" }}></div> */}
+      <Word paragraph={paragraph} />
+      <div style={{ height: "50vh" }}></div>
+      {/* <Character paragraph={paragraph} /> */}
+      {/* <div style={{ height: "100vh" }}></div> */}
+    </main>
+  );
+}

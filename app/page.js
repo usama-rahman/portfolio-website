c3199e65b80ecc08003bd268aa2c054ee9f1e97b
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-10 16:09:45 +0600

    Hallo

--- app/page.js
+++ app/page.js
@@ -1,3 +1,8 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import Preloader from "../components/Preloader";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar";
@@ -5,8 +10,26 @@ import About from "@/components/about/About";
 import Projects from "@/components/Projects/Projects";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = "default";
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
+  }, []);
+
   return (
     <main style={{ backgroundColor: "#f2f2f2" }}>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
       <Navbar />
       <Hero />
       <Projects />

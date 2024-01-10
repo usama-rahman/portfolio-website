85f3c4bce7aae7cf3d6dffd03b6efcc5d3ceb179
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-10 15:51:19 +0600

    Projects

--- app/page.js
+++ app/page.js
@@ -2,22 +2,15 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar";
 import About from "@/components/about/About";
-import Projects from "@/components/Projects";
+import Projects from "@/components/Projects/Projects";
 
 export default function Home() {
   return (
-    <main
-      // style={{ backgroundImage: "url(/Stars.png)" }}
-      // className="relative flex flex-col justify-between w-full min-h-screen bg-black "
-      style={{ backgroundColor: "#f2f2f2" }}
-    >
+    <main style={{ backgroundColor: "#f2f2f2" }}>
       <Navbar />
-      {/* <Header /> */}
       <Hero />
       <Projects />
       <About />
-      {/* <Contact /> */}
-      {/* <Blog /> */}
       <Footer />
     </main>
   );
